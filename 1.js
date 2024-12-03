ce4349a898840f4445dbabc57c4559074a3d8f1b
Tina <tina.bekkholt@hotmail.com>
2024-12-03 13:51:18 +0100

    text

--- 1.js
+++ 1.js
@@ -1,1 +1,12 @@
-console.log("hi");
+console.log();
+
+fetch('1.txt').then(response => {
+    if (!response.ok) {
+        throw new Error (`ERROR`);
+    } 
+    return response.text();
+}).then(data => {
+    console.log(data);
+}).catch(error =>
+    console.error('ERROR 2')
+);
