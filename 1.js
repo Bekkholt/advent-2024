67e4537230746659c5fe0591bd8c9a6b9cc5aece
Tina <tina.bekkholt@hotmail.com>
2024-12-03 14:12:48 +0100

    numbers

--- 1.js
+++ 1.js
@@ -1,12 +1,25 @@
-console.log();
-
 fetch('1.txt').then(response => {
     if (!response.ok) {
         throw new Error (`ERROR`);
     } 
     return response.text();
 }).then(data => {
-    console.log(data);
+    const list1 = [];
+    const list2 = [];
+    const dataLines = data.split('\n');
+    console.log(dataLines.length);
+    for (const line of dataLines) {
+        const numbers = line.split('   ');
+        console.log(numbers.length);
+        list1.push(Number.parseInt(numbers[0]));
+        list2.push(Number.parseInt(numbers[1]));
+    }
+    console.log(list1);
+    console.log(list2);
+
+
+
+
 }).catch(error =>
     console.error('ERROR 2')
 );
